81ae7a38f342b08420cf193d64b662f45bcb5963
Георгий <dfffgfgf@yandex.ru>
2021-09-20 14:56:57 +0300

    fix algo

--- main.py
+++ main.py
@@ -1,37 +1,28 @@
 import json
 
 
-def max_key(graph):
-    max_value = -1
-    for key in graph.keys():
-        if int(key) > max_value:
-            max_value = int(key)
-    return int(max_value)
-
-
 def check(start, graph, color):
     color[start] = 1
     for item in graph[str(start)]:
         if str(item) in graph.keys():
-            if color[item] == 0:
-                check(item, graph, color)
-            if color[item] == 1:
+            if color[str(item)] == 0:
+                check(str(item), graph, color)
+            if color[str(item)] == 1:
                 return False
     color[start] = 2
 
 
 def check_cycle(graph):
-    n = max_key(graph)
-    color = [0] * (n + 1)
+    color = {key: 0 for key in graph.keys()}
     for key in graph.keys():
-        if color[int(key)] == 0:
+        if color[key] == 0:
             if check(int(key), graph, color) is not None:
                 return True
     return False
 
 
 if __name__ == '__main__':
-    with open('config.json') as file:
+    with open('config1.json') as file:
         dict_tmp = json.load(file)
     if check_cycle(dict_tmp):
         print('Цикл есть')
